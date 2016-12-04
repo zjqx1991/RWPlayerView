c9142dedd125dee7049c01cee8abe05a51b768f2
紫荆秋雪 <zjqx1991@163.com>
2016-12-04 19:46:49 +0800

    初始化项目

---
+++ RWPlayerView/RWPlayerView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  RWPlayerView
+//
+//  Created by 紫荆秋雪 on 16/12/4.
+//  Copyright © 2016年 紫荆秋雪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ RWPlayerView/RWPlayerView/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  RWPlayerView
+//
+//  Created by 紫荆秋雪 on 16/12/4.
+//  Copyright © 2016年 紫荆秋雪. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
